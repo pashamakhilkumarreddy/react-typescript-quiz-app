918073bc4e6111a69b581dfeda14464b13bae623
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2020-07-18 23:33:22 +0530

    add client

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import AppHeader from '../src/components/common/Header';
+import AppFooter from '../src/components/common/Footer';
+
+function App() {
+  return (
+    <>
+      <AppHeader />
+        <main className="App">
+        </main>
+      <AppFooter />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/common/Header/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Header = () =>
+  <header></header>
+
+export default Header;
